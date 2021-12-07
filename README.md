f8497ddd1e3506d98469a354cfecb5b7b58d9961
Darito <davidalonso991@gmail.com>
2021-12-07 07:51:09 -0600

    Arreglo de bugs de busqueda y boton buscar

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Biblioteca
-Una pagina web que te permite añadir informacion de libros con uso de prototipos en JavaScript.
+Una pagina web que te permite añadir informacion de libros creada con uso de prototipos en JavaScript y manejo del DOM.
+
--- index.js
+++ index.js
@@ -128,20 +128,22 @@ function EliminarElementoConId(id) {
     mostrarNotificacion("Eliminado exitosamente");
   }, 300);
 }
-const creacionDeElemento = (libro) => {
+const creacionDeElemento = (libro, conBotonCerrar) => {
   let div = document.createElement("div");
   div.classList.add("book");
   div.id = "libro" + libro.id;
   let botonParaEliminar = document.createElement("button");
-  botonParaEliminar.classList.add("botonEliminarLibro");
-  botonParaEliminar.id = libro.id;
-  botonParaEliminar.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>';
+  if (conBotonCerrar) {
+    botonParaEliminar.classList.add("botonEliminarLibro");
+    botonParaEliminar.id = libro.id;
+    botonParaEliminar.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>';
 
-  botonParaEliminar.addEventListener("click", (e) => {
-    let idDelElementoAEliminar = e.path[2].id;
-    EliminarElementoConId(idDelElementoAEliminar);
-  });
+    botonParaEliminar.addEventListener("click", (e) => {
+      let idDelElementoAEliminar = e.path[2].id;
+      EliminarElementoConId(idDelElementoAEliminar);
+    });
+  }
   let titulo = document.createElement("p");
   titulo.classList.add("tituloDelLibro");
   titulo.innerText = libro.nombre;
@@ -154,8 +156,9 @@ const creacionDeElemento = (libro) => {
   let yaLoLei = document.createElement("p");
   yaLoLei.classList.add("yaLoLei");
   yaLoLei.innerText = libro.leido;
-
-  div.appendChild(botonParaEliminar);
+  if (conBotonCerrar) {
+    div.appendChild(botonParaEliminar);
+  }
   div.appendChild(titulo);
   div.appendChild(autor);
   div.appendChild(numeroDePaginas);
@@ -172,7 +175,7 @@ function ActualizarLibrosMostrados() {
       '<div><p class="textoDeFondo">No has agregado ningun libro aun. </p></div>';
   }
   miBiblioteca.map((libro) => {
-    let div = creacionDeElemento(libro);
+    let div = creacionDeElemento(libro, true);
     contenedorDeListaDeLibros.appendChild(div);
   });
 }
@@ -210,18 +213,22 @@ formulario.addEventListener("keypress", (e) => {
 
 const botonBuscar = document.getElementById("botonBuscar");
 const inputBuscar = document.getElementById("inputBuscar");
+let estaEscribiendo = true
 inputBuscar.addEventListener("focusin", () => {
   if (inputBuscar.value === "") {
+    console.log(inputBuscar)
     botonBuscar.style = "display: inline-flex;";
     botonBuscar.classList.add("aparecer");
     setTimeout(() => {
       botonBuscar.classList.remove("aparecer");
     }, 300);
   }
+  estaEscribiendo= true
 });
 inputBuscar.addEventListener("focusout", (e) => {
+  estaEscribiendo = false
   setTimeout(() => {
-    if (inputBuscar.value === "") {
+    if (inputBuscar.value === "" && !estaEscribiendo) {
       botonBuscar.style = "display: inline-flex;";
       botonBuscar.classList.add("desaparecer");
       setTimeout(() => {
@@ -248,7 +255,7 @@ function AgregarLibrosEncontrados(librosEncontrados) {
   if (librosEncontrados.length !== 0) {
     busquedaDeLibrosEncontrados.style = "display: block;";
     librosEncontrados.map((libro) => {
-      let div = creacionDeElemento(libro);
+      let div = creacionDeElemento(libro, false);
       contenedorDeLibrosEncontrados.appendChild(div);
     });
   } else {
@@ -275,10 +282,12 @@ function validarBusqueda(busqueda) {
 
 botonBuscar.addEventListener("click", (e) => {
   e.preventDefault();
+  limpiarContenedorDeElementoConId("contenedorDeLibrosEncontrados");
   validarBusqueda(inputBuscar.value);
 });
 
 const botonCerrarBusquedas = document.getElementById("botonCerrarBusquedas");
 botonCerrarBusquedas.addEventListener("click", () => {
   busquedaDeLibrosEncontrados.style = "";
+  limpiarContenedorDeElementoConId("contenedorDeLibrosEncontrados");
 });
